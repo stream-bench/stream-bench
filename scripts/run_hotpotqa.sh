6437f895886e047cb68da5b17acdca7a9f15e3fa
Brian Wu <brian.wu@appier.com>
2024-08-15 16:34:56 +0700

    feat(ckwu): implement toolbench and hotpotqa scripts

---
+++ scripts/run_hotpotqa.sh
@@ -0,0 +1,5 @@
+python -m stream_bench.pipelines.run_bench \
+    --agent_cfg "configs/agent/self_stream_icl.yml" \
+    --bench_cfg "configs/bench/hotpotqa.yml" \
+    --entity "photocopier" \
+    --use_wandb
